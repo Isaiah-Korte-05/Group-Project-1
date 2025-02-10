d4401d30e741f920bdef1f12de033673a6990836
Isaiah-Korte-05 <ijkmmf@gmail.com>
2025-02-10 14:28:31 -0600

    updated test output

--- src/mu/edu/libraryTest/LibraryApp.java
+++ src/mu/edu/libraryTest/LibraryApp.java
@@ -7,36 +7,77 @@ public class LibraryApp {
 
 	public static void main(String[] args) {
 
+		// Creates new library
+		System.out.println("Creating library...");
 		Library library = new Library();
+		if(library != null) {
+			System.out.println("Library created successfully.\n");
+		}
 
 		// Creates and assigns book1 to library
+		System.out.println("Adding books...");
 		Book book1 = new Book("1984", "George Orwell", "1234", 25.98);
-		library.addBook(book1);
+		if(!(library.addBook(book1))) {
+			System.out.println("Error adding book1.\n");
+		}
+		System.out.println("Book1 added successfully.\n");
+		
 
 		// Creates and assigns book2 to library
 		Book book2 = new Book("The Hunger Games", "Suzzane Collins", "5678", 20.53);
-		library.addBook(book2);
+		if(!(library.addBook(book2))) {
+			System.out.println("Error adding book2.\n");
+		}
+		System.out.println("Book2 added successfully.\n");
 
 		// Creates and assigns book3 to library
 		Book book3 = new Book("The Fellowship of the Ring", "J.R.R. Tolkien", "9012", 31.24);
-		library.addBook(book3);
+		if(!(library.addBook(book3))) {
+			System.out.println("Error adding book3.\n");
+		}
+		System.out.println("Book3 added successfully.\n");
+		
+		System.out.println("All books added successfully.\n");
+		
+		System.out.println("Searching for book with ISBN: " + book2.getISBN());
 
 		// Searches library for book matching ISBN identifier of book2
 		Book found = library.searchByISBN(book2.getISBN());
+		if(found != null) {
+			// Prints out the found book
+			System.out.println("---Book Found---\n");
+			System.out.println(found.toString());
+		}
+		else {
+			System.out.println("Book could not be found.\n");
+		}
 
-		// Prints out the found book
-		System.out.println("---Found Book---\n");
-		System.out.println(found.toString());
+		// Prints updated library after all additions and deletions
+		System.out.println("\n---Current Library---\n");
+		library.displayBooks();
 
+		System.out.println("Removing book: " + found.toString() + "\n");
+		
+		// Deletes book found during ISBN search
+		if(library.removeBook(found)) {
+			System.out.println("Book removed successfully.\n");
+		}
+		else {
+			System.out.println("Cannot remove book: \"" + found.toString() + "\", book was not found.\n");
+		}
+		
 		// Creates new book 'imaginary'
 		Book imaginary = new Book("Wind and Truth", "Brandon Sanderson", "3456", 12.12);
-
-		// Deletes book found during ISBN search
-		@SuppressWarnings("unused")
-		boolean deletedReal = library.removeBook(found);
+		
+		System.out.println("Removing book: " + imaginary.toString() + "\n");
+		
 		// Attempts to delete 'imaginary'
-		@SuppressWarnings("unused")
-		boolean deletedFake = library.removeBook(imaginary);
+		if(library.removeBook(imaginary)) {
+			System.out.println("Book removed successfully.\n");
+		}
+		else {
+			System.out.println("Cannot remove book: \"" + imaginary.toString() + "\", book was not found.\n");
+		}
 
 		// Prints updated library after all additions and deletions
 		System.out.println("\n---Current Library---\n");
